b1c8a6c3218842068321d5e829f02fabb30634db
87123-AWAISAHMED <229434215+syedabdulrahmaan22-dev@users.noreply.github.com>
2025-09-17 07:02:29 +0530

    Final fixes for schema, types, and package.json

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,27 +2,55 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-  // This defines a table named "files"
   files: defineTable({
-    // You likely have these fields already from the tutorial
     teamId: v.string(),
     document: v.string(),
     fileName: v.string(),
     createdBy: v.string(),
     archive: v.boolean(),
     whiteboard: v.string(),
-
-    // This is the new field we are adding for the canvas
     canvasData: v.optional(v.string()),
   })
-  .index("by_teamId", ["teamId"]), // Add a comma here
+  .index("by_teamId", ["teamId"]),
 
-  // Add the new teams table definition here
   teams: defineTable({
     teamName: v.string(),
     createdBy: v.string(),
   }),
+
+  // Add this new user table definition
+  user: defineTable({
+    name: v.string(),
+    email: v.string(),
+    image: v.string(),
+  }),
 });
+
+// import { defineSchema, defineTable } from "convex/server";
+// import { v } from "convex/values";
+
+// export default defineSchema({
+//   // This defines a table named "files"
+//   files: defineTable({
+//     // You likely have these fields already from the tutorial
+//     teamId: v.string(),
+//     document: v.string(),
+//     fileName: v.string(),
+//     createdBy: v.string(),
+//     archive: v.boolean(),
+//     whiteboard: v.string(),
+
+//     // This is the new field we are adding for the canvas
+//     canvasData: v.optional(v.string()),
+//   })
+//   .index("by_teamId", ["teamId"]), // Add a comma here
+
+//   // Add the new teams table definition here
+//   teams: defineTable({
+//     teamName: v.string(),
+//     createdBy: v.string(),
+//   }),
+// });
 
 // import { defineSchema, defineTable } from "convex/server";
 // import { v } from "convex/values";
